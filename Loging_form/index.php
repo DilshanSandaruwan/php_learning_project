d8058d2fd6fa17697bbe4df39a80c646b2e3234c
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-09-27 12:19:36 +0530

    update index.php

--- Loging_form/index.php
+++ Loging_form/index.php
@@ -1,7 +1,13 @@
 <?php
     include_once 'header.php';
 ?>
-    <h1>Hello !</h1>
+    <h1>Hello <?php
+    if (isset($_SESSION["username"])) {
+        echo $_SESSION["username"] . '!';
+    }else{
+        echo 'user !';
+    }
+    ?></h1>
     <p>welcome to my simple website</p>
 
     <?php
