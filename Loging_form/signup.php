c7a4a00a1acc2402ca9beeeb567885f6e96f3158
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-09-27 13:08:10 +0530

    create error massages in sign up page

--- Loging_form/signup.php
+++ Loging_form/signup.php
@@ -3,15 +3,34 @@
 ?>
     <div class="form">
         <h1>Simple Webpage</h1>
-    <form action="include/login.inc.php" method="post">
-    <input type="text" id="fname" name="firstname" placeholder="Name">
-    <input type="text" id="fname" name="firstname" placeholder="Emial">
-    <input type="text" id="fname" name="firstname" placeholder="User name">
-    <input type="text" id="fname" name="firstname" placeholder="Password">
-    <input type="text" id="fname" name="firstname" placeholder="Retype Password">
-    <button name="submit" type="submit">Login</button>
+    <form action="include/signup.inc.php" method="post">
+    <input type="text" id="fname" name="name" placeholder="Name">
+    <input type="text" id="fname" name="email" placeholder="Emial">
+    <input type="text" id="fname" name="uid" placeholder="User name">
+    <input type="text" id="fname" name="pwd" placeholder="Password">
+    <input type="text" id="fname" name="pwdrepeat" placeholder="Retype Password">
+    <button name="submit" type="submit">Register</button>
     </form>
     
+    <?php 
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyInput") {
+           echo '<div class="error">Fill in the all fields</div>';
+        }elseif ($_GET["error"] == "invalidUid") {
+            echo '<div class="error">Invalid UserName !</div>';
+        }elseif ($_GET["error"] == "invalidEmail") {
+            echo '<div class="error">Invalid Email !</div>';
+        }elseif ($_GET["error"] == "PasswordNotMatch") {
+            echo '<div class="error">Password not match !</div>';
+        }elseif ($_GET["error"] == "UidArlredyTaken") {
+            echo '<div class="error">User name Alredy taken !</div>';
+        }elseif ($_GET["error"] == "stmtfailed") {
+            echo '<div class="error">Something went wrong</div>';
+    }elseif ($_GET["error"] == "none") {
+        echo '<div class="error">Account created</div>';
+}
+}
+    ?>
     <p>Alredy have an account ?<a href="login.php">Log in</a></p>
     </div>
 <?php
