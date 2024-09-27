0e582d5c541460878342d8c379791a035114175b
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-09-27 12:17:05 +0530

    implement function to signup page

--- Loging_form/include/fucntion.inc.php
+++ Loging_form/include/fucntion.inc.php
@@ -45,6 +45,78 @@ function pwdMatch($pwd,$pwdRepeat){
 }
 
 
-function udiExists($conn,$username,$email){
-    $sql = "SELECT * FROM users WHERE usersUid";
+function uidExists($conn,$username,$email){
+    $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        header("Location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt,"ss",$username,$email);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultData)) {
+        return $row;
+    }else{
+        return false;
+    }
+
+    mysqli_stmt_close($stmt);
 }
+
+    function createUser($conn,$name,$email,$username,$pwd){
+        
+        $sql = "INSERT INTO users(usersName,usersEmail,usersUid,usersPwd) VALUE (?,?,?,?);";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt,$sql)) {
+            header("Location: ../signup.php?error=stmtfailed");
+            exit();
+        }
+
+        $hashPwd = password_hash($pwd,PASSWORD_DEFAULT);
+        mysqli_stmt_bind_param($stmt,"ssss",$name,$email,$username,$hashPwd);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+        header("Location: ../signup.php?error=none");
+        exit();
+
+    }
+
+    function emptyInputLogin($username,$pwd){
+        $result;
+        if (empty($username) || empty($pwd)) {
+           $result = true;
+        }else{
+            $result = false;
+        }
+    
+        return $result;
+    }
+
+    function LoginUsers($conn,$username,$pwd){
+        $uidExists =uidExists($conn,$username,$username);
+
+        if ($uidExists === false) {
+            header("Location: ../signp.php?error=wrongLogin");
+            exit();
+        }
+
+        $pwdHashed = $uidExists["usersPwd"];
+        $chekPwd = password_verify($pwd,$pwdHashed);
+
+        if ($chekPwd === false) {
+            header("Location: http://localhost/myphp/signup.php?error=wrongLogin");
+            exit();
+        }elseif ($chekPwd === true) {
+           session_start();
+           $_SESSION ['userid'] = $uidExists["usersId"];
+           $_SESSION ['userUid'] = $uidExists["usersUid"];
+           $_SESSION ['username'] = $uidExists["usersName"];
+           header("Location: ../index.php");
+           exit();
+        }
+    }
